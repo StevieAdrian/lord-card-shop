0dfea8ea319c9d515396ffec5497138357847359
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2025-05-02 22:00:28 +0700

    feat: add card logic

---
+++ lord-card-shop/Controller/CardController.cs
@@ -0,0 +1,36 @@
+﻿using lord_card_shop.Handler;
+using lord_card_shop.Helper;
+using lord_card_shop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lord_card_shop.Controller
+{
+    public class CardController
+    {
+        public static string AddCard(string cardName, decimal cardPrice, string cardDesc, string cardType, byte[] isFoil)
+        {
+            var error = new List<string>
+            {
+                CardValidateHelper.validateName(cardName),
+                CardValidateHelper.validatePrice(cardPrice),
+                CardValidateHelper.validateDescription(cardDesc),
+                CardValidateHelper.validateType(cardType),
+                // CardValidateHelper.validateFoil(isFoil) *tar uncomment tgg udh validasinya, biar gk lupa
+            };
+
+            for (int i = 0; i < error.Count; i++)
+            {
+                if (error[i] != null)
+                {
+                    return error[i];
+                }
+            }
+
+            CardHandler.AddCard(cardName, cardPrice, cardDesc, cardType, isFoil);
+            return null;
+        }
+    }
+}

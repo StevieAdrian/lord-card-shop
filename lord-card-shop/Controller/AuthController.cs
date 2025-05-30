bfe131f80cbc267c198e349a185d5d5e9d837523
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2025-05-30 10:42:59 +0700

    feat: add auth logic & cookie helper

---
+++ lord-card-shop/Controller/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using lord_card_shop.Handler;
+using lord_card_shop.Helper;
+using lord_card_shop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lord_card_shop.Controller
+{
+    public class AuthController
+    {
+        public static string AuthenticateUser(string username, string password, bool rememberMe)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return "Username and password must be filled!";
+            }
+
+            User user = LoginHandler.GetUser(username, password);
+
+            if (user == null)
+            {
+                return "Invalid username or password!";
+            }
+
+            SessionHelper.Session(user);
+
+            if (rememberMe)
+            {
+
+            }
+
+            return "Login Succecss";
+        }
+    }
+}

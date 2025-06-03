bfeb5766f9d6ccbedf17fcae18ea7712e8508495
joeyma4862 <111038041+joeyma4862@users.noreply.github.com>
2025-06-03 23:49:06 +0700

    fix: model structure

--- lord-card-shop/Model/Model.Designer.cs
+++ lord-card-shop/Model/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\user\Desktop\Kuliah\Semester 4\PSD_Lab\lord-card-shop\lord-card-shop\Model\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\joeym\Downloads\lord-card-shop-master\lord-card-shop\Model\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
